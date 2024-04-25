2bf0c7177faab18c910158d1caa0e0b87ee3f098
Viky <xbukovska@stuba.sk>
2024-04-26 00:22:29 +0200

    home not done yet just forgot to commit + chnage user info not working yet

---
+++ plantiesAPI/resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <section class="green">
+        <h1><span class="green">Shop</span> by Category</h1>
+        <hr />
+    </section>
+@endsection
